1503842dffc601ccf4a65494caf071f29b30b595
khehraje <52760657+khehraje@users.noreply.github.com>
2019-07-26 23:25:34 -0400

    Add files via upload

---
+++ assets/data/myBooks.ts
@@ -0,0 +1,14 @@
+import {booksInfo} from '../../app/booksInfo';
+import { HtmlTagDefinition } from '@angular/compiler';
+
+export const MYBOOKS: booksInfo[] = 
+[
+{sbookn:'The Fault in our stars', sauthor: 'John Green', sgenre: 'Young Adult Fiction', syearpub: 2012 , spic :'assets/images/TFIOS.jpg'},
+{sbookn:'In Search Of Lost Time', sauthor: 'Gabriel García Márquez', sgenre: 'Modern Literature', syearpub: 1913, spic: 'assets/images/ISOLT.jpg'},
+{sbookn:'Hamlet', sauthor: 'William Shakespeare', sgenre: 'Tragedy', syearpub: 1603, spic: 'assets/images/HMLT.jpg'},
+{sbookn:'The Notebook', sauthor: 'Nicholas Sparks', sgenre: 'Romantic', syearpub: 1996 , spic: 'assets/images/NTBK.jpg'}
+
+
+
+
+]
